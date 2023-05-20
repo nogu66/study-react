d6b462674c4cb93e1a7333f363e77da03f3dfd1b
nogu66 <ezochi66@gmail.com>
2023-05-20 16:21:34 +0900

    ♻️ ダブルクォートに変換

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import { Footer } from '../components/Footer';
-import { Main } from '../components/Main';
-import { Header } from '../components/Header';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Footer } from "../components/Footer";
+import { Main } from "../components/Main";
+import { Header } from "../components/Header";
 
 export default function Home() {
 	return (
