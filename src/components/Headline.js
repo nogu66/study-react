8549aaa76bde7dbe82dfa04bdc9a1a2eead11d6a
nogu66 <ezochi66@gmail.com>
2023-05-20 13:54:35 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ src/components/Headline.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Footer } from '../components/Footer';
+import { Links } from '../components/Links';
+
+export function Headline(props) {
+	return (
+		<div>
+			<h1 className={styles.title}>{props.page} Page</h1>
+
+			<p className={styles.description}>
+				Get started by editing{' '}
+				<code className={styles.code}>pages/{props.page}.js</code>
+			</p>
+		</div>
+	);
+}
