e9648a79fa5dc2b54e885343dcb8c115951f9a06
nogu66 <ezochi66@gmail.com>
2023-05-21 16:01:40 +0900

    🎨 Custom Hooksを使ってうまくまとめました

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,16 +3,33 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
 
-export default function Home() {
+export default function About() {
+	const { count, isShow, handleClick, handleDisplay } = useCounter();
+	const { text, array, handleChange, handleAdd } = useInputArray();
+	useBgLightBlue();
+
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>About Page</title>
 			</Head>
-			<Header/>
-			<Main page="about"/>
-			<Footer/>
+			<Header />
+			{isShow ? <h1>{count}</h1> : null}
+			<button onClick={handleClick}>ボタン</button>
+			<button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+			<input type="text" value={text} onChange={handleChange} />
+			<button onClick={handleAdd}>追加</button>
+			<ul>
+				{array.map((item) => {
+					return <div key={item}>{item}</div>;
+				})}
+			</ul>
+			<Main page="about" />
+			<Footer />
 		</div>
 	);
 }
