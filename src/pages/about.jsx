38d3ebc12ac3b420b87a8115a3d9f558cb88e2b6
nogu66 <ezochi66@gmail.com>
2023-05-21 15:50:51 +0900

    Revert "🔧 jsconfigファイルの修正"
    This reverts commit 58f4a9f505c9541a3da8bb952473ee947f1e4b36.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,36 +1,18 @@
 import Head from "next/head";
-import styles from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
-import { Main } from "src/components/Main";
-import { Header } from "src/components/Header";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import styles from "../styles/Home.module.css";
+import { Footer } from "../components/Footer";
+import { Main } from "../components/Main";
+import { Header } from "../components/Header";
 
-export default function About() {
-	const { count, isShow, handleClick, handleDisplay } = useCounter();
-	const { text, array, handleChange, handleAdd } = useInputArray();
-	useBgLightBlue();
-
+export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
 				<title>About Page</title>
 			</Head>
-			<Header />
-			{isShow ? <h1>{count}</h1> : null}
-			<button onClick={handleClick}>ボタン</button>
-			<button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-
-			<input type="text" value={text} onChange={handleChange} />
-			<button onClick={handleAdd}>追加</button>
-			<ul>
-				{array.map((item) => {
-					return <div key={item}>{item}</div>;
-				})}
-			</ul>
-			<Main page="about" />
-			<Footer />
+			<Header/>
+			<Main page="about"/>
+			<Footer/>
 		</div>
 	);
 }
