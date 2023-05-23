5d71fdfd992d97d3759cc105a15f40ac70ff1151
nogu66 <ezochi66@gmail.com>
2023-05-23 20:44:03 +0900

    ✨ コンポーネント感のStateのリフトアップについて勉強

--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,13 +1,13 @@
-import classes from 'src/components/Headline/Headline.module.css';
+import classes from "src/components/Headline/Headline.module.css";
 
 export function Headline(props) {
 	return (
 		<div>
 			<h1 className={classes.title}>{props.page} Page</h1>
-
 			<p className={classes.description}>
-				Get started by editing {props.children}
+				アイテムの数は {props.children} 個です
 			</p>
+			<button onClick={props.handleReduce}>減らす</button>{" "}
 		</div>
 	);
 }
