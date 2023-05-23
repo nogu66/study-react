f0191f235777c8bf36da28dc7a395ea63754b132
nogu66 <ezochi66@gmail.com>
2023-05-23 21:26:39 +0900

    ✨ useMemoについて学びました

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,6 +6,7 @@ import { Header } from "../components/Header";
 
 export default function About({
 	count,
+	doubleCount,
 	isShow,
 	handleClick,
 	handleDisplay,
@@ -20,9 +21,9 @@ export default function About({
 				<title>About Page</title>
 			</Head>
 			<Header />
-			{isShow ? <h1>{count}</h1> : null}
 			<button onClick={handleClick}>ボタン</button>
 			<button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+			{isShow ? <h1>{doubleCount}</h1> : null}
 			<hr />
 			<input type="text" value={text} onChange={handleChange} />
 			<button onClick={handleAdd}>追加</button>
